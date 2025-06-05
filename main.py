9e31a17903649b97b357da9de27f12de78bcaaa5
0FelipeMeira <cmeiragames@gmail.com>
2025-06-05 19:44:26 -0300

    feat: implement endpoint to save dengue dataset to SQLite database

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #
 #   Run Project:
+#   pip install -r requirements.txt
 #   uvicorn main:app --host 0.0.0.0 --port 8000
 #
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
+import sqlite3
 import httpx
 import pandas as pd
 import os
@@ -14,29 +16,57 @@ BASE_API_URL = "https://info.dengue.mat.br/api/alertcity"
 
 @app.get("/dengue-dataset/", tags=["done"])
 async def get_dengue_dataset():
-    """
-    Endpoint para ler e retornar todos os dados do arquivo dengue-dataset.csv.
-    """
     file_path = os.path.join(os.getcwd(), "dengue-dataset.csv")
 
-    # Verificar se o arquivo existe
     if not os.path.exists(file_path):
         raise HTTPException(
             status_code=404, detail="Arquivo 'dengue-dataset.csv' não encontrado"
         )
 
     try:
-        # Ler o arquivo CSV usando Pandas
         df = pd.read_csv(file_path)
 
         df = df.fillna("N/A")
         df = df.replace([float("inf"), float("-inf")], "N/A")
 
-        # Retornar os dados como JSON
         return JSONResponse(content=df.to_dict(orient="records"))
     except Exception as e:
         raise HTTPException(
             status_code=500, detail=f"Erro ao ler o arquivo CSV: {str(e)}"
+        )
+
+
+@app.post("/dengue-dataset/save/", tags=["new"])
+async def save_dengue_dataset_to_db():
+    csv_path = os.path.join(os.getcwd(), "dengue-dataset.csv")
+    db_path = os.path.join(os.getcwd(), "dengue_data.db")
+
+    if not os.path.exists(csv_path):
+        raise HTTPException(
+            status_code=404, detail="Arquivo 'dengue-dataset.csv' não encontrado"
+        )
+
+    try:
+        df = pd.read_csv(csv_path)
+        df = df.fillna("N/A")
+        df = df.replace([float("inf"), float("-inf")], "N/A")
+
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+
+        table_name = "dengue_data"
+
+        df.to_sql(table_name, conn, if_exists="replace", index=False)
+
+        conn.commit()
+        conn.close()
+
+        return {"message": "Dados salvos no banco de dados com sucesso."}
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Erro ao salvar os dados no banco de dados: {str(e)}",
         )
 
 
